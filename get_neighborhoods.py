344869408d476f573701a1e08de17cd0470acf8b
Omar Ahmed <omarka@gmail.com>
2018-06-04 09:10:19 -0700

    Added additional statistics to the get_neighborhoods.py script, per Bruce's suggestion

--- get_neighborhoods.py
+++ get_neighborhoods.py
@@ -3,12 +3,6 @@ import numpy as np
 import scipy
 import sklearn
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
-from tabulate import tabulate
-#from kmodes.kmodes import KModes
-
-from sklearn import decomposition
 
 
 # Define file name
@@ -17,9 +11,19 @@ file_name_911 = '911-calls.csv'
 # Import file as a pandas data frame
 data_frame_911 = pd.read_csv(file_name_911)
 
-#Set numpy seed
+# Print neighborhoods
 neighborhoods = data_frame_911['Neighborhood'].unique()
 
-print(neighborhoods)
+print(neighborhoods.size)
 
-# Remove frames with scores < 0
+#Reduce dataframe to only neighborhood and response time info
+crime_neighborhood_response_df = data_frame_911[['Neighborhood', 'Response Time (sec)']].dropna(subset=['Response Time (sec)'])#.groupby('Neighborhood').count().iloc[:,0]
+#Get mean response time and count statistics associated with each neighborhood
+crime_neighborhood_response_incidents = crime_neighborhood_response_df.groupby(['Neighborhood']).agg(['mean', 'count'])
+
+#Print data to terminal
+print(crime_neighborhood_response_incidents)
+
+#Create bar graph
+crime_neighborhood_response_incidents.plot(kind='bar')#camis_count.plot(kind='hist',bins=20)
+plt._show()
